edf254e3c0fd230c147edc7152e2e1d1a7e2478d
Rajesh-Royal <rajeshroyal896@gmail.com>
2022-07-03 22:59:07 +0530

    feat: list all the shifts by date group, calculate their duration

--- client/src/components/shifts/myShifts/index.tsx
+++ client/src/components/shifts/myShifts/index.tsx
@@ -1,39 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ISingleShift } from "../../../api/controllers/get-all-shifts";
 import "../../../styles/components/myShifts.scss";
+import { checkIfDateIsTodayOrTomorrow, convertMillisecondsToHourAndMinute, convertMillisecondsToMonthNameAndDay, getTotalDurationOfShifts } from "../../../util/utilityFunctions";
 
 interface IMyShiftsProps {
   shiftsData: ISingleShift[];
  }
 const MyShifts = ({ shiftsData }: IMyShiftsProps) => {
+  const [shiftGroups, setShiftGroups] = useState({})
+  console.log('shiftGroups', shiftGroups)
   useEffect(() => {
     // this gives an object with dates as keys
-const groups = shiftsData.reduce((dateGroups, shift) => {
-  const date = new Date(shift.startTime).getDay().toString();
-  if (!dateGroups[date as typeof dateGroups[keyof typeof dateGroups]]) {
-    // @ts-ignore
-    dateGroups[date] = [];
-  }
-  // @ts-ignore
-  dateGroups[date].push(shift);
-  return dateGroups;
-}, {});
-console.log('groups', groups)
-  
-  }, [])
+    const groupShiftsByDate = shiftsData.filter((sft) => !sft.booked).reduce((dateGroups, shift) => {
+      const date = convertMillisecondsToMonthNameAndDay(shift.startTime);
+      if (!dateGroups[date as typeof dateGroups[keyof typeof dateGroups]]) {
+        // @ts-ignore
+        dateGroups[date] = [];
+      }
+      // @ts-ignore
+      dateGroups[date].push(shift);
+      return dateGroups;
+    }, {});  
+    setShiftGroups(groupShiftsByDate)
+  }, [shiftsData])
   
   return (
     <div className="shifts-container">
-      {shiftsData.filter((sft) => !sft.booked).map((shift) => {
+      {Object.keys(shiftGroups).map((shift) => {
         return <>
-          <h3  className="shift-group">Today <span className="shift-data">2 shifts, 4 h</span></h3>
-          <div className="shift-details">
-            <div className="shift-timing">
-              <p className="time">12:00-14:00</p>
+          <h3 className="shift-group">{checkIfDateIsTodayOrTomorrow(shift)} <span className="shift-data">{
+            // @ts-ignore
+            shiftGroups[shift].length
+          } shifts, {
+              // @ts-ignore
+              getTotalDurationOfShifts(shiftGroups[shift])
+            }</span></h3>
+          {
+            // @ts-ignore
+            shiftGroups[shift].map((shift: ISingleShift) => { 
+              return <div className="shift-details">
+                <div className="shift-timing">
+                  <p className="time">{convertMillisecondsToHourAndMinute(shift.startTime)}-{convertMillisecondsToHourAndMinute(shift.endTime)}</p>
                   <p className="city">{shift.area}</p>
-            </div>
-            <button className="btn-pink">Cancel</button>
-          </div>
+                </div>
+                <button className="btn-pink">Cancel</button>
+              </div>
+            })
+          }
         </>
        })}
     </div>
