2d7f5b318654b4b6cd349e5f8f51813a143e8009
Rajesh-Royal <rajeshroyal896@gmail.com>
2022-07-03 23:57:00 +0530

    feat: list all available shifts

--- client/src/components/shifts/availableShifts/index.tsx
+++ client/src/components/shifts/availableShifts/index.tsx
@@ -1,11 +1,48 @@
+import { useEffect, useState } from "react";
 import { ISingleShift } from "../../../api/controllers/get-all-shifts";
+import { checkIfDateIsTodayOrTomorrow, convertMillisecondsToHourAndMinute, convertMillisecondsToMonthNameAndDay } from "../../../util/utilityFunctions";
 
 interface IAvailableShiftsProps {
   shiftsData: ISingleShift[];
- }
-const AvailableShifts = ({shiftsData}: IAvailableShiftsProps) => {
+}
+ interface IShiftGroupsType { [key: string]: ISingleShift[] }
+const AvailableShifts = ({ shiftsData }: IAvailableShiftsProps) => {
+  const [shiftGroups, setShiftGroups] = useState<IShiftGroupsType>({})
+  useEffect(() => {
+    // this gives an object with dates as keys
+    const groupShiftsByDate = shiftsData.reduce((dateGroups, shift) => {
+      const date = checkIfDateIsTodayOrTomorrow(convertMillisecondsToMonthNameAndDay(shift.startTime));
+      if (!dateGroups[date]) {
+        dateGroups[date] = [];
+      }
+      dateGroups[date].push(shift);
+      return dateGroups;
+    }, {} as IShiftGroupsType);  
+    setShiftGroups(groupShiftsByDate)
+  }, [shiftsData])
   return (
-    <div>available shifts</div>
+    <div className="shifts-container">
+      {Object.keys(shiftGroups).map((shift) => {
+        return <>
+          <h3 className="shift-group">{shift}</h3>
+          {
+            
+            shiftGroups[shift].map((shift: ISingleShift) => { 
+              return <div className="shift-details">
+                <div className="shift-timing">
+                  <p className="time">{convertMillisecondsToHourAndMinute(shift.startTime)}-{convertMillisecondsToHourAndMinute(shift.endTime)}</p>
+                </div>
+                <div className={`booking-status ${shift.booked ? "booked" : "overlapping"}`}>{shift.booked ? "Booked" : "Overlapping"}</div>
+                <button className={`${shift.booked ? "btn-pink" : "btn-green"}`}>{!shift.booked ? "Book" : "Cancel"}</button>
+              </div>
+            })
+          }
+        </>
+      })}
+      {
+        Object.keys(shiftGroups).length === 0 && <p className="no-shifts">No shifts found!</p>
+      }
+    </div>
   )
 }
 
