d488c80feb935225437afd154c94369154028fe6
Rajesh-Royal <rajeshroyal896@gmail.com>
2022-07-03 21:23:42 +0530

    style: shift-group and shift-details block styles created

--- client/src/components/shifts/myShifts/index.tsx
+++ client/src/components/shifts/myShifts/index.tsx
@@ -3,14 +3,14 @@ import "../../../styles/components/myShifts.scss";
 
 const MyShifts = () => {
   return (
-    <div className="my-shifts">
-      <h3 title="date-time" className="heading-bold">Today <span className="shift-data">2 shifts, 4 h</span></h3>
-      <div title="shift-details" className="shift-details">
+    <div className="shifts-container">
+      <h3  className="shift-group">Today <span className="shift-data">2 shifts, 4 h</span></h3>
+      <div className="shift-details">
         <div className="shift-timing">
           <p className="time">12:00-14:00</p>
           <p className="city">Helsinki</p>
         </div>
-        <button>Cancel</button>
+        <button className="btn-pink">Cancel</button>
       </div>
     </div>
   )
