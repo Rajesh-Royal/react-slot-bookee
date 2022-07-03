d68ab0114d980423b81b924f23b922eef8499787
Rajesh-Royal <rajeshroyal896@gmail.com>
2022-07-03 14:35:19 +0530

    feat: header component created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,22 @@
-import { useEffect } from 'react';
-import { getListOfAllShifts } from './api/controllers/get-all-shifts';
+import { useState } from 'react';
+import Header from './components/header';
+import { navBarItems } from './constants';
+import "./styles/utility/flexbox.scss";
 import "./styles/utility/utility.scss";
 
 function App() {
-  useEffect(() => {
-    getListOfAllShifts().then((data) => {
-      console.log(data);
-    }).catch((err) => {
-      console.log(err);
-    });
-  }, [])
+  // useEffect(() => {
+  //   getListOfAllShifts().then((data) => {
+  //     console.log(data);
+  //   }).catch((err) => {
+  //     console.log(err);
+  //   });
+  // }, [])
+  const [activeTab, setActiveTab] = useState(navBarItems.MY_SHIFTS);
   
   return (
-    <div className="App">
-      <p className="text-green">this is the green heading</p>
+    <div className="app-container">
+      <Header activeTab={activeTab} setActiveTab={setActiveTab} />
     </div>
   );
 }
